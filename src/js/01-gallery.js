0dd3b7c5611788e172ee802844ff605aa64dee40
Piotr Frontczak <piotrf2023@gmail.com>
2024-04-08 17:03:29 +0200

    update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,34 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+const galleryContainer = document.querySelector('.gallery');
+
+function createGalleryItem({ preview, original, description }) {
+  const galleryItem = document.createElement('li');
+  galleryItem.classList.add('gallery__item');
+
+  const galleryLink = document.createElement('a');
+  galleryLink.classList.add('gallery__link');
+  galleryLink.href = original;
+
+  const galleryImage = document.createElement('img');
+  galleryImage.classList.add('gallery__image');
+  galleryImage.src = preview;
+  galleryImage.setAttribute('alt', description); 
+
+  galleryLink.appendChild(galleryImage);
+  galleryItem.appendChild(galleryLink);
+
+  return galleryItem;
+}
+
+function initializeLightbox() {
+  new SimpleLightbox('.gallery a', {captionsData: 'alt', captionDelay: 250
+  });
+}
+
+galleryContainer.append(...galleryItems.map(createGalleryItem));
+
+initializeLightbox();
+
 console.log(galleryItems);
