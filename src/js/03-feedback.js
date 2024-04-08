622e04693ecef471b4d2df9ac6f959d0f1a5d234
Piotr Frontczak <piotrf2023@gmail.com>
2024-04-08 15:06:37 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,16 @@
+const form = document.querySelector("form.feedback-form");
 
+const formDataStr = localStorage.getItem("feedback-form-state");
+if (formDataStr) {
+    const formData = JSON.parse(formDataStr);
+    form.elements.email.value = formData.email;
+    form.elements.message.value = formData.message;
+}
+
+form.addEventListener("input", (ev) => {
+    const data = {
+        email: ev.target.elements.email.value,
+        message: ev.target.elements.message.value,
+    }
+    localStorage.setItem("feedback-form-state", JSON.stringify(data));
+});
