732514fd71713a4b6755b5a85e8e68244f4fb1d5
Piotr Frontczak <piotrf2023@gmail.com>
2024-04-08 14:26:01 +0200

    js created

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+import Player from "@vimeo/player";
 
+const iframe = document.querySelector("iframe#vimeo-player");
+const player = new Player(iframe);
+
+const currentTime = localStorage.getItem("currentTime");
+if (currentTime) {
+    player.setCurrentTime(currentTime);
+}
+
+player.on('timeupdate', (data) => {
+    localStorage.setItem("currentTime", data.seconds);
+})
